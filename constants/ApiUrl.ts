6a9e9b683e9c542e00d5694ccb0bb3bc6ebc97d8
Christiaan Dames <36244641+HUSKUT@users.noreply.github.com>
2022-11-10 15:09:17 +0100

    big brain fix

--- constants/ApiUrl.ts
+++ constants/ApiUrl.ts
@@ -3,19 +3,18 @@ import { API_URL } from '@env';
 
 let ApiEnv;
 
-if ( API_URL == undefined) {
+if (API_URL == undefined) {
   ApiEnv = 'https://bami-backend.preview.marnixah.com/';
 } else {
   ApiEnv = API_URL;
 }
 
-const ApiUrl = ApiEnv.toString();
+const ApiUrl = ApiEnv
+
 
 if (ApiUrl.slice(-1) !== '/')
   throw new Error(
     'API_URL environment variable must end with a slash. Please update your environment.'
   );
-
-// If last character is not a slash, raise an error
 
 export default ApiUrl;
