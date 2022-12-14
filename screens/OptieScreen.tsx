c68fed3eca77102c52bf499cb20b16f8230d25f0
Student David Vonck <dvonck3870@student.landstede.nl>
2022-12-14 10:18:12 +0100

    update

---
+++ screens/OptieScreen.tsx
@@ -0,0 +1,20 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function ProfileScreen() {
+  return (
+    <View style={styles.main}>
+      <Text>ProfileScreen</Text>
+      //
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    paddingTop: 45,
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+});
