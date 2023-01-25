e8640d7e7acb1161a42349e3488623a636ba3d32
Student David Vonck <dvonck3870@student.landstede.nl>
2023-01-25 13:18:46 +0100

    chat and chatmessage fixed

--- types/Chat.ts
+++ types/Chat.ts
@@ -3,7 +3,12 @@ import Company from './Company';
 import User from './User';
 
 export default interface Chat {
+  Id: number;
+  CreateDate: Date;
+  ModifyDate?: Date;
+  CompanyId: number;
   Company: Company;
+  StudentId: number;
   Student: User;
-  ChatMessages: ChatMessage;
+  ChatMessages: ChatMessage[];
 }
--- types/ChatMessage.ts
+++ types/ChatMessage.ts
@@ -1,3 +1,13 @@
+import Chat from "./Chat";
+import User from "./User";
+
 export default interface ChatMessage {
+  Id: Number;
+  CreateDate: Date;
+  ModifyDate?: Date;
+  ChatId: Number;
+  Chat: Chat;
+  SenderId: number;
+  Sender: User;
   Message: string;
 }
