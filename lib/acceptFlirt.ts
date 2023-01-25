5fc08f28956947f439d9d84b0554cc36b22a34ce
HUSKUT <36244641+HUSKUT@users.noreply.github.com>
2023-01-25 14:35:45 +0100

    Update lib/acceptFlirt.ts
    Co-authored-by: marnixah <marnix@roboco.dev>

--- lib/acceptFlirt.ts
+++ lib/acceptFlirt.ts
@@ -1,6 +1,5 @@
 import backendFetch from './backendFetch';
 
 export default async function acceptFlirt(flirtId: number): Promise<void> {
-  console.log('flirtid', flirtId);
   await backendFetch('POST', `flirts/${flirtId}/accept`, {});
 }
