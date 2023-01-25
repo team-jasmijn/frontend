aad062131b9fe4b487822e41bda12c7189e2e5c8
David <104158497+Sparky018@users.noreply.github.com>
2023-01-25 14:07:37 +0100

    Update lib/getChats.ts
    Co-authored-by: marnixah <marnix@roboco.dev>

--- lib/getChats.ts
+++ lib/getChats.ts
@@ -2,5 +2,5 @@ import backendFetch from './backendFetch';
 import Chat from '../types/Chat';
 
 export default async function getChats(): Promise<Chat[] | null> {
-  return await backendFetch<Chat[] | null>('GET', 'chat/'); // returns Chats
+  return await backendFetch<Chat[]>('GET', 'chat/'); // returns Chats
 }
